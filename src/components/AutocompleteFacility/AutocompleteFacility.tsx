fca96cb2ac32eedf531a0f1461902af32e5631a0
Euryn <crossdeltavi@gmail.com>
2023-01-28 11:01:50 +0100

    Update AutocompleteFacility.tsx

--- src/components/AutocompleteFacility/AutocompleteFacility.tsx
+++ src/components/AutocompleteFacility/AutocompleteFacility.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, SyntheticEvent, useCallback } from "react";
+import { FC, SyntheticEvent } from "react";
 import {
   Autocomplete,
   TextField,
@@ -14,52 +14,45 @@ type AutocompleteFacilityProps = {
   facility: Facility;
   onFacilityChange: (next_facility: Facility) => void;
 };
-export const AutocompleteFacility: FC<AutocompleteFacilityProps> =
-  memo(
-    (props) => {
-      const { facility, onFacilityChange } = props;
+export const AutocompleteFacility: FC<AutocompleteFacilityProps> = (
+  props,
+) => {
+  const { facility, onFacilityChange } = props;
 
-      const handleChange = useCallback(
-        (
-          event: SyntheticEvent<Element, Event>,
-          value: Facility | null,
-          reason: AutocompleteChangeReason,
-        ): void => {
-          if (value === null) {
-            return;
-          }
-          onFacilityChange(value);
-        },
-        [],
-      );
+  const handleChange = (
+    event: SyntheticEvent<Element, Event>,
+    value: Facility | null,
+    reason: AutocompleteChangeReason,
+  ): void => {
+    if (value === null) {
+      return;
+    }
+    onFacilityChange(value);
+  };
 
-      return (
-        <Autocomplete
-          fullWidth
-          disableClearable
-          options={AssetFacilities}
-          value={facility}
-          onChange={handleChange}
-          isOptionEqualToValue={(option, value) => {
-            return option.label === value.label;
-          }}
-          renderOption={(props, option) => {
-            return (
-              <AutocompleteOption
-                key={option.label}
-                LIprops={props}
-                option={option}
-              />
-            );
-          }}
-          filterOptions={filterOptions}
-          renderInput={(params) => {
-            return <TextField {...params} label="Facility" />;
-          }}
-        />
-      );
-    },
-    (prev, next) => {
-      return prev.facility.label === next.facility.label;
-    },
+  return (
+    <Autocomplete
+      fullWidth
+      disableClearable
+      options={AssetFacilities}
+      value={facility}
+      onChange={handleChange}
+      isOptionEqualToValue={(option, value) => {
+        return option.label === value.label;
+      }}
+      renderOption={(props, option) => {
+        return (
+          <AutocompleteOption
+            key={option.label}
+            LIprops={props}
+            option={option}
+          />
+        );
+      }}
+      filterOptions={filterOptions}
+      renderInput={(params) => {
+        return <TextField {...params} label="Facility" />;
+      }}
+    />
   );
+};
