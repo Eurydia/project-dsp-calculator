2aae44ee42acc1aae8280579b4d75b029aeba617
Euryn <crossdeltavi@gmail.com>
2024-05-02 22:19:30 +0200

    Updates

---
+++ assets/@floating-ui-dqiQ_6eD.js
@@ -0,0 +1,1 @@
+import"./react-HyB_XKJz.js";import"./react-dom-Wm5p_jAf.js";
--- assets/sorter.mts-RHRkJXms.js
+++
@@ -1,2 +0,0 @@
-const o={},s=r=>r in o?o[r]:{label:"Uh oh",workConsumptionMW:1,idleConsumptionMW:1},e=(r,t,n)=>{o[r]={label:r,workConsumptionMW:t,idleConsumptionMW:n}};e("Sorter Mk.I",.018,.009);e("Sorter Mk.II",.036,.009);e("Sorter Mk.III",.072,.009);e("Pile Sorter",.144,.009);export{s};
-//# sourceMappingURL=sorter.mts-RHRkJXms.js.map
