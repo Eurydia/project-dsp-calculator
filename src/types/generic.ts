b864297266fee781e05e5a31df99fa0490a12448
Euryn <crossdeltavi@gmail.com>
2024-06-28 03:06:59 +0200

    bumped version

--- src/types/generic.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export type InfoDefinition<T> = {
-	icon: ReactNode;
-	label: string;
-	render: (t: T) => ReactNode;
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/project-dsp-calculator/",
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		visualizer({ open: true }),
+	],
 	build: {
 		sourcemap: "inline",
 		rollupOptions: {
