0a78a04f29e83ed1f6546b7aa95894edb1d49f0b
Euryn <crossdeltavi@gmail.com>
2024-06-28 23:40:55 +0200

    added small documentation for useString hook

--- src/hooks/useString.ts
+++ src/hooks/useString.ts
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+/**
+ * @version 2.6.0
+ * @description
+ * A generic hook for strings.
+ * Whenever the state changes, the new state is also saved to local storage using the given key.
+ *
+ * I can probably use useEffect hook to archieve the same result, but this implementation is nice, concise and readable, so there is no incentive to refactor for now.
+ */
 export const useString = (
 	key: string,
 	init: string,
