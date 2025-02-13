32e6eaedc45dcc214983dbfed38f59c247b78926
Eurydia <crossdeltavi@gmail.com>
2025-02-13 10:57:35 +0100

    manually resolved security warning from dependabots and removed dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,32 @@
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/project-dsp-calculator/",
-	plugins: [
-		react(),
-		tsconfigPaths(),
-		visualizer({ open: true }),
-	],
+  base: "/project-dsp-calculator/",
+  plugins: [react(), tsconfigPaths()],
 
-	build: {
-		minify: "terser",
-		cssMinify: "lightningcss",
-		rollupOptions: {
-			treeshake: true,
-			output: {
-				minifyInternalExports: true,
-				manualChunks: (id) => {
-					if (id.includes("node_modules")) {
-						if (
-							id.includes("@mui") ||
-							id.includes("@emotion")
-						) {
-							return "vendor_mui";
-						}
-						if (id.includes("react")) {
-							return "vendor_react";
-						}
-						return "vendor";
-					}
-				},
-			},
-		},
-	},
+  build: {
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        minifyInternalExports: true,
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("@mui") ||
+              id.includes("@emotion")
+            ) {
+              return "vendor_mui";
+            }
+            if (id.includes("react")) {
+              return "vendor_react";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
